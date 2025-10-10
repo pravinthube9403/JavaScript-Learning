93af4e57a08555f6604992e14010dddff2a89de3
pravinthube9403 <104641086+pravinthube9403@users.noreply.github.com>
2025-10-10 10:04:42 +0000

    conver of data types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,37 @@
+let score = 33;
+let score1 = "33";
+let score3 = "33abc";
+
+console.log(typeof score);
+console.log(typeof score);
+
+console.log(typeof score1);
+console.log(typeof score1);
+
+let valueInNumber1 = Number(score1);
+
+let valueInNumber2 = Number(score3);
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1; false=>0;
+
+let isLoggedIn = "fsds";
+
+let boleanLoggedIn = Boolean(isLoggedIn);
+console.log(boleanLoggedIn);
+
+//  1=>true 0=>false
+//  ""=>false
+//  "dssx"=>true
+
+let someNumber=33;
+let StringNumber=String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
+
+
