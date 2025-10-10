b27ba07071b0ecfe231bddbf02a8157531829b3d
pravinthube9403 <104641086+pravinthube9403@users.noreply.github.com>
2025-10-10 09:26:49 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) //we are using nodeJs,not Browser
+
+// tc.39.es  main website of js
+let age = 18;
+
+// number 2 to 53
+// bigint
+//string=> " "
+//bolean => true false
+// null => standalone value
+// undefined =>
+// symbol=> unique
+
+//object
+
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+
---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+const accId=12345;
+let accHolderName="pravin";
+var accPass=21212;
+accCity="maharastra"
+let accStatus;
+
+accHolderName="thube"
+accPass=33333;
+
+console.table([accHolderName,accPass,accStatus,accId])
