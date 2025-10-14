d5d2e948a558f3152794f8c8e9e8d714b7c38c06
pravinthube9403 <104641086+pravinthube9403@users.noreply.github.com>
2025-10-14 08:51:06 +0000

    #

--- 07_Projects/01_Project/script.js
+++ 07_Projects/01_Project/script.js
@@ -4,5 +4,19 @@ const body = document.querySelector("body")
 // console.log(buttons);
 
 buttons.forEach((btn) => {
-   
+
+    btn.addEventListener("click", (e) => {
+        if (e.target.id === "grey") {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === "white") {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === "blue") {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === "yellow") {
+            body.style.backgroundColor = e.target.id
+        }
+    })
 })
